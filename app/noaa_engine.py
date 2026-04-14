82f2d71e084d3f34e05d79656c6778d46d4ec42d
Toanuvo <kzeezavadlov@yahoo.com>
2026-04-14 13:21:08 -0700

    Create noaa_engine.py

---
+++ app/noaa_engine.py
@@ -0,0 +1,49 @@
+import requests
+import datetime
+import json
+#Weather station at the PDX Airport
+latitude = '45.5958'
+longitude = '-122.6093'
+office = 'PQR'
+gridX = '115'
+gridY = '106'
+
+# URL and query elements for the NOAA Web site
+base_url = 'https://api.weather.gov/gridpoints/'
+full_url = base_url + office + '/' + gridX + ',' + gridY + '/forecast'
+
+
+
+def get_noaa_data():
+    # GET the response from the NWS server
+    response = requests.get(full_url)
+    
+    # Format the response object as JSON
+    data = response.json()
+    data = data["properties"]["periods"][0]
+    
+    temp = data["temperature"]
+    prec = data["probabilityOfPrecipitation"]["value"]
+    
+    # parse forcast
+    forcast = data["detailedForecast"].lower()
+    sun = "sun" in forcast
+    cloud =  "cloud" in forcast
+    rain = "rain" in forcast
+    
+    sunpct = 0
+    if sun and not cloud:
+         sunpct = 100
+    elif sun and cloud:
+        sunpct = 50
+    elif not rain and cloud:
+        sunpct = 25
+    elif rain and cloud:
+        sunpct = 0
+        
+    return {
+        "temp": temp,
+        "prec": prec,
+        "sunpct": sunpct,
+    }
+    
